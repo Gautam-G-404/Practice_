b30a83eca426c8b17e57f4352fd66fb0b759a527
Gautam-G-404 <gautam0g6@gmail.com>
2024-11-08 12:02:30 +0530

    07-11-24

--- program5.cpp
+++ program5.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+// #include<bits/stdc++.h>
 // using namespace std;
 // int fre_no(vector<int> & a,int n)
 // {
@@ -33,3 +33,31 @@
 //     }
 //     fre_no(a,n);
 // }
+
+#include<bits/stdc++.h>
+using namespace std;
+int fre_no(vector<int>& a,int n)
+{
+unordered_map <int,int> map_name;
+for(int i=0;i<n;i++)
+{
+    map_name[a[i]]++;
+}
+for(auto x:map_name)
+{
+cout<<x.first<<" "<<x.second<<endl;
+}
+}
+int main()
+{
+    int n;
+    cout<<"ENter n :"<<endl;
+    cin>>n;
+    vector <int> a(n);
+    cout<<"enter array elements: "<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    fre_no(a,n);
+}
